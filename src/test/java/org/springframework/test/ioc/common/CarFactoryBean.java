0c9c077b2a4ce2dbc33351af48548ff5566c2795
zhouruxuan <1205295974@qq.com>
2023-05-01 16:50:46 +0800

    factoryBean

---
+++ src/test/java/org/springframework/test/ioc/common/CarFactoryBean.java
@@ -0,0 +1,25 @@
+package org.springframework.test.ioc.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.test.ioc.bean.Car;
+
+@Getter
+@Setter
+public class CarFactoryBean implements FactoryBean<Car> {
+
+    private String brand;
+
+    @Override
+    public Car getObject() throws Exception {
+        Car car = new Car();
+        car.setBrand(brand);
+        return car;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+}
