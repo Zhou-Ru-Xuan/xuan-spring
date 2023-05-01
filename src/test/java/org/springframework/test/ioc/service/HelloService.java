272e5df6c14fb09ced304f61458522a52f503c43
zhouruxuan <1205295974@qq.com>
2023-05-01 16:35:34 +0800

    aware

--- src/test/java/org/springframework/test/ioc/service/HelloService.java
+++ src/test/java/org/springframework/test/ioc/service/HelloService.java
@@ -1,14 +1,36 @@
 package org.springframework.test.ioc.service;
 
+import lombok.Getter;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 /**
  * @author zhouruxuan
  * @description
  * @date 2023-05-01
  **/
-public class HelloService {
+@Getter
+public class HelloService implements ApplicationContextAware, BeanFactoryAware {
 
-	public String sayHello() {
-		System.out.println("hello");
-		return "hello";
-	}
+    private ApplicationContext applicationContext;
+
+    private BeanFactory beanFactory;
+
+    public String sayHello() {
+        System.out.println("hello");
+        return "hello";
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 }
