7c5d433e8dae4b4c31e14d7421928e75a9005ebf
zhouruxuan <1205295974@qq.com>
2023-05-02 16:32:30 +0800

    ProxyFactory

--- src/main/java/org/springframework/aop/AdvisedSupport.java
+++ src/main/java/org/springframework/aop/AdvisedSupport.java
@@ -4,34 +4,44 @@ import org.aopalliance.intercept.MethodInterceptor;
 
 
 public class AdvisedSupport {
+    //是否使用cglib代理
+    private boolean proxyTargetClass = false;
 
-	private TargetSource targetSource;
+    private TargetSource targetSource;
 
-	private MethodInterceptor methodInterceptor;
+    private MethodInterceptor methodInterceptor;
 
-	private MethodMatcher methodMatcher;
+    private MethodMatcher methodMatcher;
 
-	public TargetSource getTargetSource() {
-		return targetSource;
-	}
+    public boolean isProxyTargetClass() {
+        return proxyTargetClass;
+    }
 
-	public void setTargetSource(TargetSource targetSource) {
-		this.targetSource = targetSource;
-	}
+    public void setProxyTargetClass(boolean proxyTargetClass) {
+        this.proxyTargetClass = proxyTargetClass;
+    }
 
-	public MethodInterceptor getMethodInterceptor() {
-		return methodInterceptor;
-	}
+    public TargetSource getTargetSource() {
+        return targetSource;
+    }
 
-	public void setMethodInterceptor(MethodInterceptor methodInterceptor) {
-		this.methodInterceptor = methodInterceptor;
-	}
+    public void setTargetSource(TargetSource targetSource) {
+        this.targetSource = targetSource;
+    }
 
-	public MethodMatcher getMethodMatcher() {
-		return methodMatcher;
-	}
+    public MethodInterceptor getMethodInterceptor() {
+        return methodInterceptor;
+    }
 
-	public void setMethodMatcher(MethodMatcher methodMatcher) {
-		this.methodMatcher = methodMatcher;
-	}
+    public void setMethodInterceptor(MethodInterceptor methodInterceptor) {
+        this.methodInterceptor = methodInterceptor;
+    }
+
+    public MethodMatcher getMethodMatcher() {
+        return methodMatcher;
+    }
+
+    public void setMethodMatcher(MethodMatcher methodMatcher) {
+        this.methodMatcher = methodMatcher;
+    }
 }
--- src/test/java/org/springframework/test/aop/DynamicProxyTest.java
+++ src/test/java/org/springframework/test/aop/DynamicProxyTest.java
@@ -8,6 +8,7 @@ import org.springframework.aop.TargetSource;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.framework.CglibAopProxy;
 import org.springframework.aop.framework.JdkDynamicAopProxy;
+import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.test.common.WorldServiceInterceptor;
 import org.springframework.test.service.WorldService;
 import org.springframework.test.service.WorldServiceImpl;
@@ -15,30 +16,43 @@ import org.springframework.test.service.WorldServiceImpl;
 
 public class DynamicProxyTest {
 
-	private AdvisedSupport advisedSupport;
+    private AdvisedSupport advisedSupport;
 
-	@Before
-	public void setup() {
-		WorldService worldService = new WorldServiceImpl();
+    @Before
+    public void setup() {
+        WorldService worldService = new WorldServiceImpl();
 
-		advisedSupport = new AdvisedSupport();
-		TargetSource targetSource = new TargetSource(worldService);
-		WorldServiceInterceptor methodInterceptor = new WorldServiceInterceptor();
-		MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* org.springframework.test.service.WorldService.explode(..))").getMethodMatcher();
-		advisedSupport.setTargetSource(targetSource);
-		advisedSupport.setMethodInterceptor(methodInterceptor);
-		advisedSupport.setMethodMatcher(methodMatcher);
-	}
+        advisedSupport = new AdvisedSupport();
+        TargetSource targetSource = new TargetSource(worldService);
+        WorldServiceInterceptor methodInterceptor = new WorldServiceInterceptor();
+        MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* org.springframework.test.service.WorldService.explode(..))").getMethodMatcher();
+        advisedSupport.setTargetSource(targetSource);
+        advisedSupport.setMethodInterceptor(methodInterceptor);
+        advisedSupport.setMethodMatcher(methodMatcher);
+    }
 
-	@Test
-	public void testJdkDynamicProxy() throws Exception {
-		WorldService proxy = (WorldService) new JdkDynamicAopProxy(advisedSupport).getProxy();
-		proxy.explode();
-	}
+    @Test
+    public void testJdkDynamicProxy() throws Exception {
+        WorldService proxy = (WorldService) new JdkDynamicAopProxy(advisedSupport).getProxy();
+        proxy.explode();
+    }
 
-	@Test
-	public void testCglibDynamicProxy() throws Exception {
-		WorldService proxy = (WorldService) new CglibAopProxy(advisedSupport).getProxy();
-		proxy.explode();
-	}
+    @Test
+    public void testCglibDynamicProxy() throws Exception {
+        WorldService proxy = (WorldService) new CglibAopProxy(advisedSupport).getProxy();
+        proxy.explode();
+    }
+
+    @Test
+    public void testProxyFactory() throws Exception {
+        // 使用JDK动态代理
+        advisedSupport.setProxyTargetClass(false);
+        WorldService proxy = (WorldService) new ProxyFactory(advisedSupport).getProxy();
+        proxy.explode();
+
+        // 使用CGLIB动态代理
+        advisedSupport.setProxyTargetClass(true);
+        proxy = (WorldService) new ProxyFactory(advisedSupport).getProxy();
+        proxy.explode();
+    }
 }
